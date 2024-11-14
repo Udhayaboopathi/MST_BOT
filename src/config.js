c79c33b37f196f9426493c6303bf4d47babe648b
Udhayaboopathi <udhayayboopathi2003@gmail.com>
2024-11-14 22:10:58 +0530

    Initial commit

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   prefix: "!",
   owner: "581142001739628565",
   token:
-    "MTIyNjE3MDA5MzM3Mzg4NjU1NA.GLq2k0._mYM92LFkoVBTXJ7Vw1fb5bxaSHVAo03W58hNM",
+    "MTIyNjE3MDA5MzM3Mzg4NjU1NA.Gae-Mq.9xAKVHG0heO3KhC7tUDX_nQ9cQsWvNlkLO4Bxg",
 };
